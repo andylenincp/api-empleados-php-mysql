8edc2773e81e531309bdbc54e9e54aec2e7757a3
Andy Castillo Palma <andy.castillopalma@outlook.com>
2021-10-26 22:47:41 -0500

    first commit

---
+++ empleados.sql
@@ -0,0 +1,43 @@
+--
+-- Base de datos: `empleados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `nombre`, `correo`) VALUES
+(1, 'Andy', 'andy.castillopalma@outlook.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
